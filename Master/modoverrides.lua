d25c722ad359231d62ff45e07336f75583875bf6
WBDiamond <360mlgnoscope420blazeit@gmail.com>
2017-11-15 23:27:02 +0500

    cluster 2

---
+++ Master/modoverrides.lua
@@ -0,0 +1,63 @@
+return {
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-569043634"]={ configuration_options={  }, enabled=true },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="DestroyByAll",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-816057392"]={
+    configuration_options={
+      FLIPMOON=false,
+      HIDECAVECLOCK=false,
+      HUDSCALEFACTOR=100,
+      SEASONOPTIONS="Clock",
+      SHOWBEAVERNESS=true,
+      SHOWMAXONNUMBERS=true,
+      SHOWMOON=1,
+      SHOWNAUGHTINESS=true,
+      SHOWNEXTFULLMOON=true,
+      SHOWSTATNUMBERS=true,
+      SHOWTEMPBADGES=true,
+      SHOWTEMPERATURE=true,
+      SHOWWANINGMOON=true,
+      SHOWWORLDTEMP=true,
+      UNIT="T" 
+    },
+    enabled=true 
+  } 
+}
